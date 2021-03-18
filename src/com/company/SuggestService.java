2d13faf10afadb26784e1d9ae719655dabb51b8e
Mikhail Kozlov <kozlov.mihael@yandex.ru>
2021-03-18 09:44:19 +0300

    Initial commit

---
+++ src/com/company/SuggestService.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SuggestService {
+    public SuggestService(List companyNames) {
+        //TODO: Your code
+    }
+    public List suggest(String input, Integer numberOfSuggest) {
+        return Collections.emptyList();
+        //TODO: Your code
+    }
+}
